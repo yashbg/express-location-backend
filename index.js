03e85c2749aff2610f50dcc866366e5ce3711d8b
Yash Gupta <yashbg7@gmail.com>
2022-04-13 00:17:31 +0530

    Add port and listen

--- index.js
+++ index.js
@@ -13,4 +13,14 @@ mongoose.connect('mongodb://localhost:27017/locationApp')
 
 const app = express();
 
+const port = 3000;
+
 app.use('/', locationRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello!');
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
