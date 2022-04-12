97a71a3a691489d71a29bdbe8e8c4920851fa2a9
Yash Gupta <yashbg7@gmail.com>
2022-04-12 22:56:14 +0530

    Add mongoose connection

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
-
-const PORT = 3000;
-
-// const DBConnection = require('database');
-
+const mongoose = require('mongoose');
 const locationRouter = require('./locationRouter');
+
+mongoose.connect('mongodb://localhost:27017/locationApp')
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch(err => {
+        console.log('Error in connecting to database');
+        console.log(err);
+    });
 
 const app = express();
 
