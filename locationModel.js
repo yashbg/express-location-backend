42e0d4c9409889d0298e8fd619ed6273d1c229a3
Yash Gupta <yashbg7@gmail.com>
2022-04-12 22:57:40 +0530

    Refactor location model

--- locationModel.js
+++ locationModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const LocationSchema = new mongoose.Schema({
+const locationSchema = new mongoose.Schema({
     name: String,
     latitude: Number,
     longitude: Number
 });
 
-let Location = mongoose.model('LocationSchema', LocationSchema);
+const Location = mongoose.model('Location', locationSchema);
 
 module.exports = { Location };
