4503cc947a7e960c20b5be9dc7f413b546b6e969
Yash Gupta <yashbg7@gmail.com>
2022-04-13 01:07:02 +0530

    Add bodyParser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const locationRouter = require('./locationRouter');
 
@@ -14,6 +15,12 @@ mongoose.connect('mongodb://localhost:27017/locationApp')
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.use('/', locationRouter);
 
