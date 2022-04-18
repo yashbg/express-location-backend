bef75f9f5971118c74230d0e0a0d58d025c5757e
Yash Gupta <yashbg7@gmail.com>
2022-04-19 02:30:19 +0530

    Refactor services and routes

--- routes/locationRoutes.js
+++ routes/locationRoutes.js
@@ -1,12 +1,50 @@
 const express = require('express');
 
-const { apiGetLocations, apiAddLocation, apiUpdateLocation, apiDeleteLocation } = require('../services/locationController');
+const { getLocations, addLocation, updateLocation, deleteLocation } = require('../services/locationService');
 
 const router = express.Router();
 
-router.get('/', apiGetLocations);
-router.post('/', apiAddLocation);
-router.patch('/', apiUpdateLocation);
-router.delete('/', apiDeleteLocation);
+router.get('/', (req, res) => {
+    getLocations()
+        .then(result => {
+            return res.status(result.status).json({ locations: result.locations });
+        })
+        .catch(error => {
+            return res.status(error.status).json({ message: error.message });
+        });
+});
+
+router.post('/', (req, res) => {
+    const { name, latitude, longitude } = req.body;
+    addLocation(name, latitude, longitude)
+        .then(result => {
+            return res.status(result.status).json({ message: result.message });
+        })
+        .catch(error => {
+            return res.status(error.status).json({ message: error.message });
+        });
+});
+
+router.patch('/', (req, res) => {
+    const { name, newLatitude, newLongitude } = req.body;
+    updateLocation(name, newLatitude, newLongitude)
+        .then(result => {
+            return res.status(result.status).json({ message: result.message });
+        })
+        .catch(error => {
+            return res.status(error.status).json({ message: error.message });
+        });
+});
+
+router.delete('/', (req, res) => {
+    const { name } = req.body;
+    deleteLocation(name)
+        .then(result => {
+            return res.status(result.status).json({ message: result.message });
+        })
+        .catch(error => {
+            return res.status(error.status).json({ message: error.message });
+        });
+});
 
 module.exports = router;
